4723c8a2ce6fd9d9b3760e5c8e901bfcca848412
Pratik Randive <121440105+pratikrandive04@users.noreply.github.com>
2023-02-28 23:42:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+function clearErrors(){
+    errors = document.getElementsByClassName('formError');
+    for(let item of errors){
+        item.innerHTML = "";
+    }
+}
+
+function setError(id,error){
+    element = document.getElementById(id);
+    element.getElementsByClassName('formError')[0].innerHTML = error;
+}
+
+
+function validateForm(){
+
+    var returnValue = true;
+    clearErrors();
+
+    //perform validation
+    //name validation
+    var name = document.forms["survey-form"]["name"].value;
+    if(name.length==0){
+        setError("nameDiv","*Name Can't Be Empty");
+        returnValue= false;
+    }
+    //age Validation
+    var age = document.forms["survey-form"]["age"].value;
+    if(age<0){
+        setError("ageDiv","*Age Can't Be Nagetive");
+        returnValue= false;
+    }
+    // number Validation
+    var number = document.forms["survey-form"]["number"].value;
+    if(number<0){
+        setError("numberDiv","*Number Can't Be Nagetive");
+        returnValue= false;
+    }
+    if(number>10 ||number <=9){
+        setError("numberDiv","*Enter A Valid 10 digit Number");
+        returnValue= false;
+    }
+
+    return returnValue;
+}
